f980800dceff4b5ace625e30f40233a5ac553a2d
sharpedge12 <anshul1612sharma@gmail.com>
2024-02-26 10:34:11 +0530

    first commit

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import Replicate from "replicate";
+
+
+dotenv.config();
+
+const router = express.Router();
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_KEY,
+});
+
+
+router.route('/').post(async (req, res) => {
+  try {
+    const prompt  = req.body.prompt;
+    const  nPrompt = req.body.nprompt;
+    console.log(prompt)
+    console.log(nPrompt)
+    const output = await replicate.run(
+      "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+      {
+        input: {
+          width: 1024,
+          height: 1024,
+          prompt: prompt,
+          refine: "expert_ensemble_refiner",
+          scheduler: "K_EULER",
+          lora_scale: 0.6,
+          num_outputs: 1,
+          guidance_scale: 7.5,
+          apply_watermark: false,
+          high_noise_frac: 0.8,
+          negative_prompt: nPrompt,
+          prompt_strength: 0.8,
+          num_inference_steps: 25
+        }
+      }
+    );
+    res.status(200).json({ photo: output[0]});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+  }
+});
+
+export default router;
+
+
+
---
+++ server/routes/postRoutes.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+import Post from '../mongodb/models/post.js';
+
+dotenv.config();
+
+const router = express.Router();
+
+router.route('/').get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+  }
+});
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photo,
+    });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+  }
+});
+
+export default router;
+
+
