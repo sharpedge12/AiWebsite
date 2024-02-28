984c1a0bac02239ebe0700ef132e40a789707db3
sharpedge12 <75135658+sharpedge12@users.noreply.github.com>
2024-02-28 19:23:57 +0530

    Update postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
 
 import Post from '../mongodb/models/post.js';
 
 dotenv.config();
 
 const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 router.route('/').get(async (req, res) => {
   try {
@@ -19,11 +26,13 @@ router.route('/').get(async (req, res) => {
 router.route('/').post(async (req, res) => {
   try {
     const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+    console.log(photoUrl)
 
     const newPost = await Post.create({
       name,
       prompt,
-      photo: photo,
+      photo: photoUrl.url,
     });
 
     res.status(200).json({ success: true, data: newPost });
